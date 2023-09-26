285a01bde4a98d1748ff23941fcf7cf3e4582130
Obinna <macmartins081@gmail.com>
2023-09-26 12:33:45 +0400

    Added demo code for network connectivity

--- NetworkReachabilityDemo/ViewController.swift
+++ NetworkReachabilityDemo/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var connectionStatusLabel: UILabel!
+    @IBOutlet weak var networkTypeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        networkStatusChanged()
     }
-
-
+    
+    func networkStatusChanged() {
+        NetworkReachability.shared.netStatusChangeHandler = { [weak self] in
+            guard let self = self else { return }
+            
+            if NetworkReachability.shared.isConnected {
+                if let networkType = NetworkReachability.shared.interfaceType {
+                    switch networkType {
+                    case .cellular:
+                        self.updateUIForConnected("You are connected to your mobile data")
+                    case .wifi:
+                        self.updateUIForConnected("You are connected with Wi-Fi")
+                    case .wiredEthernet:
+                        self.updateUIForConnected("You are connected with Ethernet")
+                    default:
+                        print("You are connected to some other type of network.")
+                    }
+                }
+            } else {
+                // Handle network disconnected state
+                self.updateUIForDisconnected()
+            }
+        }
+    }
+    
+    // Function to update the UI when the network is connected.
+    func updateUIForConnected(_ networkTypeString: String) {
+        DispatchQueue.main.async {
+            self.connectionStatusLabel.text = "🟢 Connected"
+            self.networkTypeLabel.text = networkTypeString
+        }
+    }
+    
+    // Function to update the UI when the network is disconnected.
+    func updateUIForDisconnected() {
+        DispatchQueue.main.async {
+            self.connectionStatusLabel.text = "🔴 Disconnected"
+            self.networkTypeLabel.text = "No network connection"
+        }
+    }
 }
-
